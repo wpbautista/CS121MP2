0f3968e68e1a0fa372dfb6a474b76d99d0803f4a
jdsalongsongan <jdsalongsongan@up.edu.ph>
2022-05-19 10:52:38 +0800

    added vuex, axios, and add product page

--- src/components/SL-AddProduct.vue
+++ src/components/SL-AddProduct.vue
@@ -3,11 +3,18 @@
     <h1 class="title">Add Product</h1>
     <section class="product-sec">
         <div class="img-upload-container">
-            
             <input type="file" @change="addingprd" id="product-img" hidden/>
             <label for="product-img" class="product-img-overlay">Upload Image</label>
             <img alt="" class="product-img-display" />
         </div>
+        <div class="product-details">
+            <h1 class="product-name" contenteditable="true" data-placeholder="Product Name">Product Name</h1>
+            <p class="product-desc" contenteditable="true" data-placeholder="Product Description">A description of the product</p>
+            <p class="product-price" contenteditable="true" data-placeholder="Product Price">Price</p>
+        </div>
+    </section>
+    <section class="detailed-details">
+        
     </section>
 </body>  
 </template>
@@ -28,6 +35,9 @@ body{
     padding: 50px 0;
 }
 
+div{
+    border: 1px red solid;
+}
 .title{
     text-align: center;
     font-size: 3rem;
@@ -37,19 +47,24 @@ body{
     opacity: 0.5;
 }
 
+.product-sec{
+    display: flex;
+}
+
 .prodct-img-display{
     background: white;
     width: 100%;
 }
 
 .img-upload-container{
-    width: 60%;
+    width: 50%;
     position: relative;
     height: 600px;
+    flex: 1;
+    margin: 10px;
 }
 
 .product-img-overlay{
-    margin-left: 40px;
     position: absolute;
     top: 0;
     left: 0;
@@ -68,5 +83,24 @@ body{
 
 .product-img-overlay:hover{
     opacity: 100%;
+}
+
+.product-details{
+    width: 50%;
+    max-width: 50%;
+    flex: 1;
+    height: 600px;
+    margin: 10px;
+}
+
+*[contenteditable="true"]{
+    outline: none;
+    border: 2px solid #cacaca;
+    padding: 10px;
+    margin-bottom: 5px;
+}
+
+*[contenteditable="true"]:focus{
+    border: 2px solid #000000;
 }
 </style>
