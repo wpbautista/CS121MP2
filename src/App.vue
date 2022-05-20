a05236305f8db79305865e1f2e5f1518034232bf
Fren <71578467+wpbautista@users.noreply.github.com>
2022-05-20 21:30:14 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+ <signup_customer />
+</template>
+
+<script>
+import signup_customer from './components/signup_customer.vue'
+export default{
+  name: 'App',
+  components : {
+    signup_customer
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
